2050c939668bc3da48768ee2a655be78bc95a109
Serhii Sydorenko <sidorchik91@gmail.com>
2023-03-21 20:15:23 +0200

    task is done. added hooks instead class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,55 +1,55 @@
-import React from 'react';
+import { useState } from 'react';
 import Section from './Section';
 import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics';
 import Notification from './Notification';
- 
-export class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+
+export const App = () => {
+  const [good, increaseGood] = useState(0);
+  const [neutral, increaseNeutral] = useState(0);
+  const [bad, increaseBad] = useState(0);
+
+  const options = ['good', 'neutral', 'bad'];
+
+  const handleFeedback = e => {
+    const targetOption = e.target.name;
+
+    switch (targetOption) {
+      case 'good':
+        increaseGood(good + 1);
+        break;
+      case 'neutral':
+        increaseNeutral(neutral + 1);
+        break;
+      case 'bad':
+        increaseBad(bad + 1);
+        break;
+      default: return 0;
+    }
   };
 
-  handleFeedback = (e) => {
-    const stateName = e.target.name;
-    this.setState(prevState => ({
-      [stateName]: prevState[stateName] + 1,
-    }));
-  };
-
-  render() {
-    const { good, neutral, bad} = this.state;
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.handleFeedback}
-          />
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions options={options} onLeaveFeedback={handleFeedback} />
+      </Section>
+      {good + neutral + bad > 0 ? (
+        <Section title={'Statistics'}>
+          <Statistics good={good} neutral={neutral} bad={bad} />
         </Section>
-        {(good + neutral + bad) > 0 ? (
-          <Section title={'Statistics'}>
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-            />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </div>
+  );
+};
